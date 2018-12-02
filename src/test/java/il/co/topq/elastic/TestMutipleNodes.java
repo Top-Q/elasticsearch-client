ab4d3ed7df5786123f38a1e850c046c737560164
itaiag <itai.agmon@gmail.com>
2018-12-02 11:51:34 +0200

    wip

--- src/test/java/il/co/topq/elastic/TestMutipleNodes.java
+++ src/test/java/il/co/topq/elastic/TestMutipleNodes.java
@@ -3,7 +3,7 @@ package il.co.topq.elastic;
 import java.io.IOException;
 import java.util.List;
 
-import org.junit.Before;
+import org.junit.Assert;
 import org.junit.Test;
 
 import il.co.topq.elastic.model.Post;
@@ -17,23 +17,29 @@ public class TestMutipleNodes extends AbstractCreateRemoveIndexTestCase {
 
 	@Test
 	public void testHighAvailiablity() throws IOException, InterruptedException {
-		addPost(555);
+		addPostAndVerify(555);
 		System.out.println("Stop the main Elasticsearch node");
-		Thread.sleep(10000);
-		addPost(666);
+		// sleep(10);
+		addPostAndVerify(666);
 		System.out.println("Restart the main Elasticsearch node and take down the secondery");
-		Thread.sleep(10000);
-		addPost(777);
+		// sleep(10);
+		addPostAndVerify(777);
 	}
 
-	private void addPost(int id) throws IOException {
+	private void addPostAndVerify(int id) throws IOException {
+		if (!client.index(INDEX).isExists()) {
+			client.index(INDEX).create(SETTINGS);
+		}
 		Post post0 = new Post();
 		post0.setId(id);
 		post0.setOp("Itai");
 		post0.setPoints(100);
 		post0.setSubreddit("all");
-		client.index(INDEX).document(DOC).add().single("100", post0);
+		client.index(INDEX).document(DOC).add().single(id + "", post0);
 		sleep(1);
+		List<Post> posts = client.index(INDEX).document(DOC).search().byTerm("id", id + "").asClass(Post.class);
+		Assert.assertNotNull(posts);
+		Assert.assertEquals(1, posts.size());
 	}
 
 }
